294b4b8096c914c8b9e81a2e0d901f940bead054
Robert (Jamie) Munro <rjmunro@arjam.net>
2026-05-06 12:01:45 +0200

    feat: add prettier.config.mjs with @ianvs/prettier-plugin-sort-imports bundled
    The plugin is imported directly as an ESM module so that Node resolves it
    from code-standard-preset's own node_modules, rather than requiring consumers
    to install it themselves.

---
+++ prettier.config.mjs
@@ -0,0 +1,32 @@
+import sortImports from '@ianvs/prettier-plugin-sort-imports'
+
+/** @type {import('prettier').Config} */
+const config = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	printWidth: 120,
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	endOfLine: 'lf',
+	trailingComma: 'es5',
+	plugins: [sortImports],
+	importOrder: [
+		'<BUILTIN_MODULES>',
+		'<TYPES>^(node:)',
+		'',
+		'<THIRD_PARTY_MODULES>',
+		'<TYPES>',
+		'',
+		'^@sofie-automation',
+		'<TYPES>^@sofie-automation',
+		'',
+		'^[./]',
+		'<TYPES>^[./]',
+	],
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+	importOrderTypeScriptVersion: '5.7.0',
+	importOrderCaseSensitive: false,
+}
+
+export default config
